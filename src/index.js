4e48e5704eed2b1db4901f3d23bfe0f262b6d757
Олег Бидихов <obidikhov@>
2022-08-02 12:44:50 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -5,12 +5,3 @@ import App from './components/App.vue'
 new Vue({
   render: h => h(App)
 }).$mount("#app")
-
-
-var arr1 = [],
-    arr2 = [ 1],
-    res = arr1.filter(item => !arr2.includes(item));
-console.log(res);
-console.log(Boolean(!arr1.length));
-
-console.log(arr2.length);
